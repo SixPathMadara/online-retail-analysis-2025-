6da9d920024f090e8dce54be17efa2dc8c0f4beb
Olebogeng_Legotsa <72413429+SixPathMadara@users.noreply.github.com>
2025-07-20 22:37:17 +0200

    code source added

---
+++ solution-folder[2025]/retail-analysis-solution[2025].py
@@ -0,0 +1,9 @@
+# writer: Olebogeng Legotsa
+# year: 2025
+# database: https://archive.ics.uci.edu/dataset/352/online+retail
+
+import pandas as pd 
+
+df=pd.read_excel('online retail/Online Retail.xlsx', sheet_name='Online Retail')
+
+print(df)
